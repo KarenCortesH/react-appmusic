68a99148f93a45aaaced7ab97b2ed5175d171a45
Karen Cortes <karencortes9828@gmail.com>
2022-03-24 00:33:43 -0500

    Initialize project app-music React
    Initialize project using Create React App

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <div className='header'>
+       <img src='{require("../img/MuzicLogo.png')} alt="" />
+   </div>
+    
+  )
+}
+
+export default Header
